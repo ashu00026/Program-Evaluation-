a5fb9b48757285a2b6a61d86c7dde0b2b60fe32a
ashu00026 <19341a1207@gmrit.edu.in>
2023-04-06 01:19:02 +0530

    alpha1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://panicodes.onrender.com",
+  origin: "https://panicodes.onrender.com",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
