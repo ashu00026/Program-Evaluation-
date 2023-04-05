c9d5872ea24e4e68c3c619c9d1735fdb9e6acbd4
ashu00026 <19341a1207@gmrit.edu.in>
2023-04-06 00:44:12 +0530

    alpha

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-require('dotenv').config();
-require('express-async-errors');
-const connectDB=require('./db/connect')
+require("dotenv").config();
+require("express-async-errors");
+const connectDB = require("./db/connect");
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://panicodes.onrender.com",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
-const mainRouter = require('./routes/main');
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const authMiddleware=require('./middleware/auth');
-const { connect } = require('mongoose');
+const mainRouter = require("./routes/main");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const authMiddleware = require("./middleware/auth");
+const { connect } = require("mongoose");
 
 // middleware
 // app.set('view engine','ejs')
 // app.use(express.static('./public'));
 app.use(express.json());
 
-app.use(authMiddleware)
-app.use('/api/v1', mainRouter);
+app.use(authMiddleware);
+app.use("/api/v1", mainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -34,7 +33,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.URL)
+    await connectDB(process.env.URL);
     // await studentDatabase.create({name:"rahul",year:3,results:[true,false,false,true]})
     // await staffDatabase.create({name:"sir1",subjects:["c++","java","python"]})
     app.listen(port, () =>
